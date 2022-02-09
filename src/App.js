cbe989a40e1d45bba00c952227d82d4e2551a834
root <root@technisys.com>
2022-02-08 22:49:22 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button>comidas</button>
+      <button>promos</button>
+      <button>locales</button>
+      <button>carrito</button>
+      <button>nosotros</button>
+    </div>
+  );
+}
+
+export default App;
