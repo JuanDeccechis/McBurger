730a3f333dfc37741fbd2e9dda30a4f2409d7491
root <root@technisys.com>
2022-02-13 16:15:37 -0300

    textarea for comments

--- src/pages/UsPage.jsx
+++ src/pages/UsPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from '../components/button/Button';
 import IconsCustom from '../components/icons/IconsCustom';
 import Input from '../components/input/Input';
+import TextArea from '../components/textArea/TextArea';
 
 class UsPage extends React.Component {
     constructor(props) {
@@ -37,7 +38,6 @@ class UsPage extends React.Component {
         const { selectedSection, fileAttached } = this.state;
         return (
             <article className={`container-pages us-form`}>
-us page
                 <div className="container-slider-data-categories">                    
                     <div className={`slider-data ${isMobile ? '' : 'centralized'}`}>
                         <div id={`slider-categories-0`} className="text1 us-option text-centralized" onClick={() => this.setFocus(0)}> 
@@ -50,23 +50,19 @@ us page
                 </div>
                 <div className={`divider`} >
                     <Input placeholder="Nombre" name="Nombre" minLength={3} type="text" hasIconLeft={false} >
-                        <IconsCustom icon="NavigationOutlinedIcon" className="right" />
                     </Input>
                     <Input placeholder="Apellido" name="Apellido" minLength={3} type="text" hasIconLeft={false} >
-                        <IconsCustom icon="NavigationOutlinedIcon" className="right" />
                     </Input>
                     <Input placeholder="E-mail" name="E-mail" minLength={3} type="text" hasIconLeft={false} >
-                        <IconsCustom icon="NavigationOutlinedIcon" className="right" />
                     </Input>
                     <Input placeholder="Telefono" name="Telefono" minLength={3} type="text" hasIconLeft={false} >
+                    </Input>
+                    <Input placeholder="Ciudad" name="Ciudad" minLength={3} type="text" hasIconLeft={false} >
                         <IconsCustom icon="NavigationOutlinedIcon" className="right" />
                     </Input>
-                    <Input placeholder="Telefono" name="Telefono" minLength={3} type="text" hasIconLeft={false} >
-                        <IconsCustom icon="NavigationOutlinedIcon" className="right" />
-                    </Input>
-                    <Input placeholder="Telefono" name="Telefono" minLength={3} type="text" hasIconLeft={false} >
-                        <IconsCustom icon="NavigationOutlinedIcon" className="right" />
-                    </Input>
+                    <TextArea placeholder="Comentario" name="Comentario" minLength={3} type="text" hasIconLeft={false} >
+                    </TextArea>
+                    
                 {selectedSection === 0 ?
                     <Button isDisabled={false} content="Enviar comentario" isPrimary={true} isBig={true} />            
                 :
